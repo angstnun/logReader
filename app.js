45089314e250e92b7bc51d145307dd98951c68a7
Stef Nunez <angstnun@gmail.com>
2019-11-15 00:45:10 -0500

    got vue working

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const server = require("http").createServer(app);
+const server = require('http').createServer(app);
+const path = require('path');
 const port = 80;
-const hostName = "127.0.0.1";
-const dalType = process.env.DAL_TYPE;
+const hostName = '127.0.0.1';
+const rootRouter = require('./Backend/routes')(app);
 
-app.get("/", (req, res) => {
-    
-});
+
+app.use(express.static('public'));
 
 server.listen(port, hostName, () => {
     console.log(`Listening on: http://${hostName}:${port}`);
